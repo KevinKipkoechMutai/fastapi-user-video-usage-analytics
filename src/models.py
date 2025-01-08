4e22faff44defad055afa8f4068440bc91a77053
kevinmutai <mutaikevinkip.05@gmail.com>
2025-01-08 07:16:41 +0300

    use composite primary keys

--- src/models.py
+++ src/models.py
@@ -1,17 +1,25 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, PrimaryKeyConstraint
 from database import Base
 
 # user-group class
 class UserGroup(Base):
     __tablename__ = "user_groups"
     user_id = Column(Integer, primary_key=True)
-    group_id = Column(Integer)
+    group_id = Column(Integer, primary_key=True)
     group_name=Column(String)
+
+    __table_args__ = (
+        PrimaryKeyConstraint('user_id', 'group_id'),
+    )
 
 #user-activity class
 class UserActivity(Base):
     __tablename__ = "user_activity"
-    report_date_yyyymmdd = Column(String, primary_key=True)
+    report_date_yyyymmdd = Column(String)
     user_id = Column(Integer, ForeignKey("user_groups.user_id"), primary_key=True)
-    lecture_id = Column(Integer)
+    lecture_id = Column(Integer, primary_key=True)
     watched_seconds = Column(Integer)
+
+    __table_args__ = (
+        PrimaryKeyConstraint('user_id', 'lecture_id'),
+    )
